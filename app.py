734cd279e2e12c5c540a4ee0c579f7b4c411f4c8
hugozxcc <166377865+hugozxcc@users.noreply.github.com>
2024-04-08 15:38:26 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Endpoint 1
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Halo, ini web service simple saya'
+
+# Endpoint 2
+@app.route('/greet/<name>', methods=['GET'])
+def greet(name):
+    return f'Halo, {name}!'
+
+# nambah2
+@app.route('/greet', methods=['GET'])
+def greet_myself():
+    my_name = 'nama saya Fadil'
+    return f'Halo, {my_name}!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
